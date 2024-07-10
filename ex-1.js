152032294a276a034d887996233d18eac62519c1
PNKim <paljapon.nait@gmail.com>
2024-07-10 13:30:26 +0700

    feat: resolve ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,21 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let result = [];
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum = 0;
+    for (let j = i; j < numbers.length; j++) {
+      sum += numbers[j];
+      result.push(j);
+      if (sum > target) {
+        result = [];
+        return result;
+      } else if (sum === target) {
+        return result;
+      }
+    }
+  }
+}
+
+let numbers = [2, 7, 11, 15];
+console.log(twoSum(numbers, 9));
